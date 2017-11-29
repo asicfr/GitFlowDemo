d88205a237d8b4a423fcc394d0ad7f27cc0efae4
COULONNIER <coulonnier.nicolas.44@gmail.com>
2017-11-29 11:35:51 +0100

    add route message

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'))
 })
 
+app.get('/messagez', (req, res) => {
+    res.send('message')
+})
+
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!')
 })
