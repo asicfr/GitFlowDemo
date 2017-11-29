b8c55b467eeb548492783faebc2019e186a1a7a0
COULONNIER <coulonnier.nicolas.44@gmail.com>
2017-11-29 11:15:20 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!')
+})
