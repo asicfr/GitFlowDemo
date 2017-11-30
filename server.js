2b446cb66b6f5dd2b2c719cfd8586ef1dad8c44c
COULONNIER <coulonnier.nicolas.44@gmail.com>
2017-11-30 15:04:07 +0100

    users

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/message', (req, res) => {
     res.send('message')
 })
 
+app.get('/users', (req, res) => {
+    res.send('users')
+})
+
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!')
 })
