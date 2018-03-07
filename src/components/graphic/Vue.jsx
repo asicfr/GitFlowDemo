645d40705f4fd86045b759b25156299a6318ab14
COULONNIER <coulonnier.nicolas.44@gmail.com>
2018-03-07 09:16:48 +0100

    add color

--- src/components/graphic/Vue.jsx
+++ src/components/graphic/Vue.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
 import '../../css/Flow.css'
 
 class Vue extends Component {
@@ -24,16 +23,20 @@ class Vue extends Component {
   renderCommit(columns) {
     let y = 0
     return columns.map((row) => {
+      const styles = {
+        develop: '#6A5ACD',
+        master: '#1E90FF'
+      }
       y += 50
       return (
-        <svg key={row} y={y} fillOpacity="0" width="50" height="50">
+        <svg key={row} y={y} width="50" height="50">
           {
           this.isRowCommit(row) ?
-            <circle key={row.commit} cx="50%" cy="50%" r="40%" stroke="black" strokeWidth="3" fill="red" />
+            <circle key={row.commit} cx="50%" cy="50%" r="40%" fill={styles[row.branch]} stroke="black" strokeWidth="2" />
           :
             <circle fillOpacity="0" cx="50%" cy="50%" r="40%" strokeWidth="3" fill="red" />
         }
-          <text x="50%" y="50%" textAnchor="middle" fill="black" stroke="black" strokeWidth="2px" dy=".3em">{row.commit}</text>
+          <text x="50%" y="50%" textAnchor="middle" fill="white" dy=".32em">{row.commit}</text>
         </svg>
       )
     })
@@ -45,7 +48,7 @@ class Vue extends Component {
     return this.state.grid.columns.map((columns, i) => {
       x += 270
       return (
-        <svg key={columns[i]} fillOpacity="0" width="20vh" height="90vh" x={x} y="20">
+        <svg key={columns[i]} width="20vh" height="90vh" x={x} y="0">
           {this.renderCommit(this.state.grid.columns[i])}
         </svg>)
     })
